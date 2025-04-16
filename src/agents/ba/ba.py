5816116ce0cea57d033c712dab0136a7ae69b40d
Anshul Chauhan <anshul1.chauhan@paytm.com>
2025-04-16 12:27:40 +0530

    updated ba bot

--- src/agents/ba/ba.py
+++ src/agents/ba/ba.py
@@ -2,4 +2,7 @@ from ..base import BaseAgent
 
 class BaAgent(BaseAgent):
     def __init__(self):
-        super().__init__("ba",["current_date_tool", "execute_query_tool", "fetch_permitted_tables_tool", "fetch_permitted_schemas_tool"])
+        if ENABLE_ALL_ACCESS:
+            super().__init__("ba",["current_date_tool", "execute_query_tool", "fetch_all_datasets_tool", "fetch_schema_name_from_dataset_id_tool"])
+        else:
+            super().__init__("ba",["current_date_tool", "execute_query_tool", "fetch_permitted_tables_tool", "fetch_permitted_schemas_tool"])
--- src/constants/cdp.py
+++ src/constants/cdp.py
@@ -1,5 +1,6 @@
 import os
 
+ENABLE_ALL_ACCESS = os.environ.get("ENABLE_ALL_ACCESS", "false").lower() == "true"
 PERMITTED_DATASETS = os.environ.get("PERMITTED_DATASETS", "").split(",")
 PERMITTED_DATABASES = os.environ.get("PERMITTED_DATABASES", "").split(",")
 PERMITTED_TABLES = os.environ.get("PERMITTED_TABLES", "").split(",")
